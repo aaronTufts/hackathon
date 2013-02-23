f657df519ebe31c725000aa90b97050a0739e2d3
AaronTufts <aarontietz@gmail.com>
2013-02-23 11:28:55 -0500

    added JobHelpController

---
+++ test/test/JobHelpController.h
@@ -0,0 +1,13 @@
+//
+//  JobHelpController.h
+//  test
+//
+//  Created by Aaron Tietz on 2/22/13.
+//  Copyright (c) 2013 Aaron Tietz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JobHelpController : NSObject
+
+@end
---
+++ test/test/JobHelpController.m
@@ -0,0 +1,15 @@
+//
+//  JobHelpController.m
+//  test
+//
+//  Created by Aaron Tietz on 2/22/13.
+//  Copyright (c) 2013 Aaron Tietz. All rights reserved.
+//
+
+#import "JobHelpController.h"
+
+@implementation JobHelpController
+
+
+
+@end
