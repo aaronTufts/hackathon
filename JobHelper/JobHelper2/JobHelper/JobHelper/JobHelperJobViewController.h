b58252772c61cd27364d9e26e3ba755c6230a518
AaronTufts <aarontietz@gmail.com>
2013-02-23 15:32:42 -0500

    all three views working!

---
+++ JobHelper/JobHelper2/JobHelper/JobHelper/JobHelperJobViewController.h
@@ -0,0 +1,13 @@
+//
+//  JobHelperJobViewController.h
+//  JobHelper
+//
+//  Created by Aaron Tietz on 2/23/13.
+//  Copyright (c) 2013 j.a.m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JobHelperJobViewController : UIViewController
+
+@end
---
+++ JobHelper/JobHelper2/JobHelper/JobHelper/JobHelperJobViewController.m
@@ -0,0 +1,38 @@
+//
+//  JobHelperJobViewController.m
+//  JobHelper
+//
+//  Created by Aaron Tietz on 2/23/13.
+//  Copyright (c) 2013 j.a.m. All rights reserved.
+//
+
+#import "JobHelperJobViewController.h"
+
+@interface JobHelperJobViewController ()
+
+@end
+
+@implementation JobHelperJobViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
